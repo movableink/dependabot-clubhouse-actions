48fd8d19dfdf9e70bb14ecdf9761cc39f6f27b24
Alex LaFroscia <alex@lafroscia.com>
2019-12-12 15:58:51 -0500

    feat: create and link ClubHouse ticket on Dependabot PRs

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const createTicket = require("./lib/create-ticket");
+
+const payload = JSON.stringify(github.context.payload, undefined, 2);
+
+// Ensure we are running on a `pull_request` event
+if (!payload.pull_request) {
+  core.warning("Workflow run outside of a `pull_request` event");
+  return;
+}
+
+if (payload.action === "opened") {
+  if (!payload.pull_request.user.login.includes("dependabot")) {
+    core.debug("Not a dependabot PR");
+    return;
+  }
+
+  createTicket(payload);
+}
