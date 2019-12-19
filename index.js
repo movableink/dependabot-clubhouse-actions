01c068884b3becc8c8e592a61f1858db012069e3
Alex LaFroscia <alex@lafroscia.com>
2019-12-19 13:37:53 -0500

    chore: use shared eslint/prettier configuration

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-"use strict";
+'use strict';
 
-const core = require("@actions/core");
-const github = require("@actions/github");
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-const createTicket = require("./lib/create-ticket");
+const createTicket = require('./lib/create-ticket');
 
 const { payload } = github.context;
 
 // Ensure we are running on a `pull_request` event
 if (!payload.pull_request) {
-  core.warning("Workflow run outside of a `pull_request` event");
+  core.warning('Workflow run outside of a `pull_request` event');
   return;
 }
 
-if (payload.action === "opened") {
-  if (!payload.pull_request.user.login.includes("dependabot")) {
-    core.debug("Not a dependabot PR");
+if (payload.action === 'opened') {
+  if (!payload.pull_request.user.login.includes('dependabot')) {
+    core.debug('Not a dependabot PR');
     return;
   }
 
